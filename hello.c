a4c6d40eaadbabf2b25709890de6ff3543499415
Lisa Yan <lisayan@google.com>
2020-09-10 16:35:21 -0700

    Avoided infinite recursive call by adding no_hook variable

--- hello.c
+++ hello.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
     void *ptr = malloc(4);
     free(ptr);
-    printf("Hello World\n");
+    time_t curtime;
+    time(&curtime);
+    printf("Hello World %s\n", ctime(&curtime));
     return 0;
 }
