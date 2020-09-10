becc3787f4d28004615fec753c1be164d6f0cd5d
Lisa Yan <lisayan@google.com>
2020-09-10 16:35:44 -0700

    Avoided infinite recursive call by adding no_hook variable

--- malloc_wrappers.c
+++ malloc_wrappers.c
@@ -7,12 +7,16 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
+static __thread int no_hook;
 pthread_mutex_t lock;
 static void *(*real_malloc)(size_t s) = NULL;
 static void (*real_free)(void *) = NULL;
 static void *(*real_calloc)(size_t nelt, size_t eltsize) = NULL;
 static void *(*real_realloc)(void *b, size_t s) = NULL;
+char *message = "%s";
 
 static void init() {
   real_malloc = dlsym(RTLD_NEXT, "malloc");
@@ -26,13 +30,30 @@ static void init() {
   }
 }
 
+static void printout() {
+  //char* message;
+  time_t current_time;
+  time(&current_time);
+  //fprintf(stderr, "%s", ctime(&current_time));
+  //fputs(message, stderr);
+  fputs(ctime(&current_time), stderr);
+}
+
 void *malloc(size_t size) {
     pthread_mutex_lock(&lock);
+    void *caller;
     if (!real_malloc) {
       init();
     }
+    if (no_hook) {
+      return real_malloc(size);
+    }
+    no_hook = 1;
+    caller = __builtin_return_address(0);
     void *ptr = real_malloc(size);
     fputs("My malloc called\n", stderr);
+    printout();
+    no_hook = 0;
     pthread_mutex_unlock(&lock);
     return ptr;
 }
